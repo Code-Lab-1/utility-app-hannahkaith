979cd1847e5aacfa80769ae9ac84831cdf346229
hannahkaith <114160283+hannahkaith@users.noreply.github.com>
2023-01-07 22:39:18 +0400

    Update vending-machine.py

--- vending-machine.py
+++ vending-machine.py
@@ -1,10 +1,10 @@
 #VENDING MACHINE!
 
-#greetings
-#print method is used to show greetings and optionss
+#GREETINGS
+#print method is used to show greetings, options and responses
 print("⋆﹥━━━━━━━━━━━━━━━﹤⋆")
 print("Hey there! ♥")
-print("Can we offer you a beverage that would surely satiate your thirst? \n(Choose one of the options to answer!)")
+print("Can we offer you a beverage that would surely satiate your thirst? \n(Choose one of the options to answer!)\n")
 print("1. Yes! / 2. No thanks")
 print("⋆﹥━━━━━━━━━━━━━━━﹤⋆\n")
 
@@ -22,42 +22,42 @@ else: #should any option not be chosen, this would be printed ↓
     quit() #this immediately terminates the program
 
 
-#drink choices
+#DRINK CHOICES
 drinks = [" a. Coldwater      $2", " b. Iced Tea       $2", " c. Iced Coffee    $2\n",
 " d. Cola           $3", " e. Melon Soda     $3", " f. Banana Milk    $4"] #a list was used to store the items
-print("Pick your drink of choice!")
+print("Pick your drink of choice!\n (Choose one of the letters to answer!)")
 for drink in drinks: #'for loop' is used to print each item on a new line
      print (drink)
 
 
 
-#suggestions made for chosen drink 
+#SUGGESTIONS MADE FOR THE CHOSEN DRINK 
 #if-else method is used to print out suggestions and questions
 drink = input() #a variable to store the chosen drink
 if drink == 'a': #if 'a' is chosen, this would be printed ↓
         print("Great choice! this would pair nicely with a yanyan!")
         print("Would you like to buy a snack?\n\n 1. Yes! / 2. No thanks")
-        drink = 'Coldwater' #the variable will then be replaced with 'Coldwater'
+        drink = 'Coldwater' #its value will then be replaced with 'Coldwater'
 elif drink == 'b':  #if 'b' is chosen, this would be printed ↓
         print("Great choice! You could pair this with a pocky nicely!")
         print("Would you like to buy a snack?\n\n 1. Yes! / 2. No thanks")
-        drink = 'Iced Tea' #the variable will then be replaced with 'Iced Tea'
+        drink = 'Iced Tea' #its value will then be replaced with 'Iced Tea'
 elif drink == 'c':  #if 'c' is chosen, this would be printed ↓
         print("Great choice! this would pair nicely with milk bread!")
         print("Would you like to buy a snack?\n\n 1. Yes! / 2. No thanks")
-        drink = 'Iced Coffee' #the variable will then be replaced with 'Iced Coffee'        
+        drink = 'Iced Coffee' #its value will then be replaced with 'Iced Coffee'        
 elif drink == 'd':  #if 'd' is chosen, this would be printed ↓
         print("Great choice! You could pair this with hazelnut wafers nicely!")
         print("Would you like to buy a snack?\n\n 1. Yes! / 2. No thanks")
-        drink = 'Cola' #the variable will then be replaced with 'Cola'
+        drink = 'Cola' #its value will then be replaced with 'Cola'
 elif drink == 'e':  #if 'e' is chosen, this would be printed ↓
         print("Great choice! this would pair nicely with a choco pie!")
         print("Would you like to buy a snack?\n\n 1. Yes! / 2. No thanks")
-        drink = 'Melon Soda' #the variable will then be replaced with 'Melon Soda'
+        drink = 'Melon Soda' #its value will then be replaced with 'Melon Soda'
 elif drink == 'f':  #if 'f' is chosen, this would be printed ↓
         print("Great choice! You could pair this with a matcha flavoured kitkat nicely!")
         print("Would you like to buy a snack?\n\n 1. Yes! / 2. No thanks")
-        drink = 'Banana Milk' #the variable will then be replaced with 'Banana Milk'
+        drink = 'Banana Milk' #its value will then be replaced with 'Banana Milk'
 else: #should any option not be chosen, this would be printed ↓
     print("Oh no! Please try again. This time, choose one of the options!")
     quit() #this immediately terminates the program 
@@ -72,7 +72,7 @@ if ans3 == '1':
     for snack in snacks: #'for loop' is used to print each item on a new line
         print (snack)
 
-    snack = input("\nPick your snack of choice!\n") #a variable to store the chosen snack
+    snack = input("\nPick your snack of choice!\n(Choose one of the numbers to answer!)\n") #a variable to store the chosen snack
     if snack == 'i': #if 'i' is chosen, it will be replaced with 'Matcha KitKat'
         snack = 'Matcha KitKat'
     elif snack == 'ii': #if 'ii' is chosen, it will be replaced with 'Choco pie'
@@ -100,15 +100,15 @@ if ans3 == '1':
     #if-else determines if there is change from the given amount
     if money == total: #if 'money' is equals to the 'total', this would be printed ↓
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆")
-        print("Enjoy your food and have a splendid day!")
-    elif money >= total: #if 'money' is greater than to the 'total', this would be printed ↓
+        print("Enjoy your food and have a splendid day!\n")
+    elif money >= total: #if 'money' is greater than the 'total', this would be printed ↓
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆")
         print(f"Your change is {change}")
-        print(f"Enjoy your {drink} and {snack}. Have a wondrous day!")
+        print(f"Enjoy your {drink} and {snack}. Have a wondrous day!\n")
         print("⋆﹥━━━━━━━━━━━━━━━﹤\n⋆")
-    elif money <= total: #if 'money' is less than to to the 'total', this would be printed ↓
+    elif money <= total: #if 'money' is less than the 'total', this would be printed ↓
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆")
-        print("Oopsies! that's not enough money :<")
+        print("Oopsies! that's not enough money :<\n")
 
 
 
@@ -119,15 +119,17 @@ elif ans3 == '2':
     change = int(money) - int(drinkchoice) #this variable stores the change
     if money == drinkchoice: #if 'money' is equals to the drinks' price, this would be printed ↓
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆")
-        print("Enjoy your food and have a splendid day!")
+        print("Enjoy your food and have a splendid day!\n")
     elif money >= drinkchoice: #if 'money' is greater than the drinks' price, this would be printed ↓
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆")
         print(f"Your change is {change}")
-        print(f"Enjoy your {drink} and have a wondrous day!")
+        print(f"Enjoy your {drink} and have a wondrous day!\n")
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆\n")
     elif money <= drinkchoice: #if 'money' is less than the drinks' price, this would be printed ↓
         print("⋆﹥━━━━━━━━━━━━━━━﹤⋆") 
-        print("Oopsies! that's not enough money :<")
+        print("Oopsies! that's not enough money :<\n")
 else: #should any option not be chosen, this would be printed ↓
     print("Oh no! Please try again. This time, choose one of the options!")
     quit() #this immediately terminates the program 
+
+#END OF PROGRAM :)
